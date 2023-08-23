e7147331fab501b29223d4104e6c71cbe0f58d1f
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-08-23 18:42:24 +0700

    create and done  component slider and viers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./index.css"
 import Login from "./components/Login";
 import Header from "./components/Header"
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
 
       </Router>
---
+++ src/components/Recommended.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Recommended = (props) => {
+  return (
+    <Container>
+      <h4>Recommended for You</h4>
+      <Content>
+
+
+      </Content>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+padding: 0 0 26px;
+`
+const Content = styled.div`
+`
+export default Recommended
